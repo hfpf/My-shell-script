aefdae3d07b63dec5cc6a4422d8784531a6deadc
hfpf <hfpf@linhuiba.com>
2018-08-08 13:43:36 +0800

    Mysql test shell

---
+++ Get_Test_Info.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+INTERVAL=5
+PREFIX=$INTERVAL-sec-status
+RUNFILE=/home/benchmarks/running
+mysql -uroot -p123456 -e 'SHOW GLOBAL VARIABLES' >> mysql-variables
+while test -e $RUNFILE; do
+    file=$(date +%F_%I)
+    sleep=$(date +%s.%N | awk "{print $INTERVAL - (\$1 % $INTERVAL)}")
+    sleep $sleep
+    ts="$(date +"TS %s.%N %F %T")"
+    loadavg="$(uptime)"
+    echo "$ts $loadavg" >> $PREFIX-${file}-status
+    mysql -uroot -p123456 -e 'SHOW GLOBAL STATUS' >> $PREFIX-${file}-status &
+    echo "$ts $loadavg" >> $PREFIX-${file}-innodbstatus
+    mysql -uroot -p123456 -e 'SHOW ENGINE INNODB STATUS\G' >> $PREFIX-${file}-innodbstatus &
+    echo "$ts $loadavg" >> $PREFIX-${file}-processlist
+    mysql -uroot -p123456 -e 'SHOW FULL PROCESSLIST\G' >> $PREFIX-${file}-processlist &
+    echo $ts
+done
+echo Exiting because $RUNFILE does not exist.
