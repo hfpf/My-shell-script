398d2bb02d408051a3daeb81d958028205d04061
huangfu <hfpf@live.cn>
2018-09-07 20:52:29 +0800

    Update slow_query.sh

--- slow_query.sh
+++ slow_query.sh
@@ -7,3 +7,9 @@
 # 2.
 # pt-query-digest用法:(慢查询日志一般保存在mysql安装目录下的data文件中，如mac慢查询日志的所在目录：/usr/local/mysql/data）
 # pt-query-digest slow.log 
+
+# 3. 一秒执行一次 SHOW GLOBAL STATUS命令捕获数据，通过计数器数据的峰谷来发现问题。
+mysqladmin ext -i1 | awk '
+/Queries/{q=$4-qp;qp=$4}
+/Threads_connected/{tc=$4}
+/Threads_running/{printf "%5d %5d %5d\n", q, tc, $4}'
